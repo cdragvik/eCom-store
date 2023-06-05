83f3254220869cb68772c08dc7c3fd8618f5396a
Cdragvik <cndragvik@gmail.com>
2023-06-05 13:43:39 +0200

    added contact page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,31 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 
 
+
 const NavBar = styled.nav `
-background-color: red;
+    background-color: #213555;
+    height: 100px;
+    display: flex;
+    flex-direction: row;
 
 `
 
+const StyledList = styled.ul`
+list-style: none;
+color: #fff;    
+`
 
 
 export default function Header() {
     return (
     <NavBar>
-        <Link to="/">Home</Link>
+        
+        <Link to="/">ONE STOP SHOP</Link>   
 
+        <StyledList>
+           <Link to="/">Home</Link> 
+           <Link to="/contact">Contact</Link>
+        </StyledList>
+        
     </NavBar>)
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import {Home} from './pages/Home.jsx';
+import Contact from './pages/contact';
 
 const router = createBrowserRouter([
   {
     path: "/", 
     element: <Home></Home>
+  }, 
+  {
+    path: "/contact", 
+    element: <Contact></Contact>
   }
+
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(

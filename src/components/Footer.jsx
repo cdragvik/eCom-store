5427e8b2054fec74cf7f9650ef144b6ee24de143
Cdragvik <cndragvik@gmail.com>
2023-06-05 14:19:51 +0200

    Added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+const FooterContainer = styled.footer`
+  background-color: #213555;
+  color: #fff;
+  padding: 20px;
+`;
+
+const FooterText = styled.p`
+  text-align: center;
+  margin: 0;
+`;
+
+export default function Footer() {
+  return (
+    <FooterContainer>
+      <FooterText>© 2023 Designed and built by Christiane Nanette Dragvik</FooterText>
+    </FooterContainer>
+  );
+};
+

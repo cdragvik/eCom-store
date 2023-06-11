33270f8d335cebdcbdd7f0a777271afd243bdfe1
Cdragvik <cndragvik@gmail.com>
2023-06-11 15:11:28 +0200

    Contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React, { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-
 import { Home } from './pages/Home';
-
 import { Checkout } from './pages/checkout';
 import { CheckoutSuccessPage } from './pages/checkoutSuccess';
-
 import { ProductPage } from './pages/ProductPage';
-import { ContactPage } from './pages/contact';
+import { ContactPage } from './pages/Contact/';
 
 
 function App() {
@@ -21,7 +17,7 @@ function App() {
           <Route path="/product/:id" element={<ProductPage shoppingCart={shoppingCart} setShoppingCart={setShoppingCart}/>} />
           <Route path="/checkout" element={<Checkout shoppingCart={shoppingCart}/>} />
           <Route path="/checkout/success" element={<CheckoutSuccessPage shoppingCart={shoppingCart} setShoppingCart={setShoppingCart}/>} />
-          <Route path="/contact" element={<ContactPage shoppingCart={shoppingCart} />} />
+          <Route path="/contact" element={< ContactPage shoppingCart={shoppingCart} />} />
         </Routes>
       </Router>
     );
